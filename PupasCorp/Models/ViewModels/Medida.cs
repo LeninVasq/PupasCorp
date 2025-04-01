128f0f18ec9ae43fd5a4ac8829b4b91f54d53b05
LeninVasq <150496561+LeninVasq@users.noreply.github.com>
2025-03-31 23:32:36 -0600

    Medidas

---
+++ PupasCorp/Models/ViewModels/Medida.cs
@@ -0,0 +1,6 @@
+﻿namespace PupasCorp.Models.ViewModels
+{
+    public class Medida
+    {
+    }
+}
--- PupasCorp/obj/Debug/net8.0/PupasCorp.AssemblyInfo.cs
+++ PupasCorp/obj/Debug/net8.0/PupasCorp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PupasCorp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89d84ec6253139d8373c24068593c91a20e36a81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acf6f3c475d8392c7bb2086999b2e0b9a12a3e7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PupasCorp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PupasCorp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
